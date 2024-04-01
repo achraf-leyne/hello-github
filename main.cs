4be48ef7d804f1d3d709041d18293c29f1ff1292
Achraf leyne <achrafln11@gmail.com>
2024-04-01 11:36:18 -0400

    initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+﻿namespace demo
+{
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitHub! Je m'appelle achraf leyne");
+        }
+    }
+}
